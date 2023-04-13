9caaccaa3e78455136ea0285b17098d8a3b9f83f
Ometere <ookuoluse@yahoo.com>
2023-04-13 15:11:21 +0100

    third commit

--- src/components/videoDetails/VideoDetails.jsx
+++ src/components/videoDetails/VideoDetails.jsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import ReactPlayer from "react-player";
+import { CheckCircle } from "@mui/icons-material";
+import { Stack, Box, Typography } from "@mui/material";
+import Videos from "../Videos";
+import { fetchFromAPI } from "../../utils/FetchFromApi";
 
 export default function VideoDetails() {
+  const [videoDetail, setVideoDetail] = useState(null);
+  const { id } = useParams();
+  useEffect(() => {
+    fetchFromAPI(`videos?part=snippet,statistics&id=${id}`).then((data) =>
+      setVideoDetail(data.items[0])
+    );
+  }, [id]);
+  const {
+    snippet: { title, channelId, channelTitle },
+    statistics: { viewCount, likeCount },
+  } = videoDetail;
   return (
-    <div>VideoDetails</div>
-  )
+    <Box minHeight="95vh">
+      <Stack direction={{ xs: "column", md: "row" }}>
+        <Box flex={1}>
+          <Box sx={{ width: "100%", position: "sticky", top: "86px" }}>
+            <ReactPlayer
+              className="react-player"
+              url={`https://www.youtube.com/watch?v=${id}`}
+              controls
+            />
+            <Typography color="white" variant="h5" fontWeight="bold" p={2}>
+              {title}
+            </Typography>
+          </Box>
+        </Box>
+      </Stack>
+    </Box>
+  );
 }
